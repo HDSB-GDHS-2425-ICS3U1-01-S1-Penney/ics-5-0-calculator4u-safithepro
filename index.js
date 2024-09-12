4625462d5a946a37dc3584c2c9a1307f32a4d833
safithepro <1ahmedsaf3@hdsb.ca>
2024-09-12 12:46:29 -0400

    I used varibales to create certain values and signs for a calculator

---
+++ index.js
@@ -0,0 +1,19 @@
+console.log("Calculator")
+
+let value1 = prompt("What is your value?")
+let value2 = prompt("What is your value?")
+
+let operation = ("What operation are you using?")
+
+if (operation = "+") {
+    addition(value1 + value2) 
+}
+if (operation = "-"){
+    subtraction(value1 - value2)
+}
+if (operation = "/"){
+    division(value1 / value2)
+}
+if (operation = "*"){
+    multiplication(value1 * value2)
+}
