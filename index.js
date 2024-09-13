fb2db37cbc8c3968417bbe183191e6de46625fc0
safithepro <1ahmedsaf3@hdsb.ca>
2024-09-13 15:10:54 -0400

    Im almost done the square operation.

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 console.log("Calculator")
 
-let operation = prompt("What operation will you use? +,-,/,*,")
+let operation = prompt("What operation will you use? +,-,/,*, square, square root")
+if (operation == "square") {
+    let numSquared = parseFloat(prompt("What number do you want squared?") 
+    console.log(numSqaured * numSqaured)
+}
 let value1 = parseFloat(prompt("What is your first value?"))
 let value2 = parseFloat(prompt("What is your second value?"))
 
- if (operation == "+") {
-   
-    console.log(value1 + value2) 
-} else if (operation == "-"){
-    
+if (operation == "+") {
+
+    console.log(value1 + value2)
+} else if (operation == "-") {
+
     console.log(value1 - value2)
-} else if (operation =="/"){
-    
+} else if (operation == "/") {
+
     console.log(value1 / value2)
-} else if (operation == "*"){
+} else if (operation == "*") {
     console.log(value1 * value2)
-}
+} 
+
+
